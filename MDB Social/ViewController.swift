6aeb2fd2c797b1beccd557e90dfb942b02aeac48
Michelle Kroll <michellekroll@berkeley.edu>
2020-10-18 13:41:41 -0700

    initial commit

---
+++ MDB Social/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MDB Social
+//
+//  Created by Michelle Kroll on 10/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
